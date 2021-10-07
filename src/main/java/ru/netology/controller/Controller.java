0ab36fd83e039fe80204f6a77aa1b016dcc2d3cc
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-10-07 15:50:12 +0300

    add method security

--- src/main/java/ru/netology/controller/Controller.java
+++ src/main/java/ru/netology/controller/Controller.java
@@ -1,20 +1,40 @@
 package ru.netology.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 public class Controller {
 
-    @GetMapping("/help")
-    public String getCityOfResidence() {
-        return "You have switched to an assistant, how can we help?";
+    @GetMapping("/read")
+    @Secured("ROLE_READ")
+    public String read() {
+        return "Hello, " + SecurityContextHolder.getContext().getAuthentication().getName()
+                + ". You went to the reading page";
     }
 
-    @GetMapping("/auth")
-    public String authorizationMenu() {
-        return "Hello, " + SecurityContextHolder.getContext().getAuthentication().getName() +
-                "! You have entered the authorized user menu";
+    @GetMapping("/write")
+    @RolesAllowed("ROLE_WRITE")
+    public String write() {
+        return "Hi, " + SecurityContextHolder.getContext().getAuthentication().getName()
+                + ". You went to the recording page";
+    }
+
+    @GetMapping("/delete")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
+    public String delete() {
+        return "Hey, " + SecurityContextHolder.getContext().getAuthentication().getName()
+                + ". You have been taken to the deletion page";
+    }
+
+    @GetMapping("/named")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String named(String username) {
+        return "Hello, " + username + ". You are redirected to the /named page";
     }
 }
--- src/main/java/ru/netology/securityConfig/SecurityConfig.java
+++ src/main/java/ru/netology/securityConfig/SecurityConfig.java
@@ -3,27 +3,39 @@ package ru.netology.securityConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication().withUser("Aleksey")
-                .password(encoder.encode("12345")).roles("ADMIN");
+        auth.inMemoryAuthentication()
+                .withUser("user_read")
+                .password(encoder.encode("12345")).roles("READ")
+
+                .and()
+
+                .withUser("user_write")
+                .password(encoder.encode("11111")).roles("WRITE")
+
+                .and()
+
+                .withUser("user_delete")
+                .password(encoder.encode("22222")).roles("DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/help").permitAll()
-                .and()
-                .authorizeRequests().anyRequest().authenticated()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin();
     }
